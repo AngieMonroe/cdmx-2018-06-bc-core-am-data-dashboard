28e0bbb21b7d9e95c19c2961cad89aad3110174c
AngieMonroe <ame.monroy@gmail.com>
2018-06-25 17:29:37 -0500

    Ajustes en funcionalidad

---
+++ src/data.js
@@ -0,0 +1,8 @@
+const link = "https://angiemonroe.github.io/cdmx-2018-06-bc-core-am-data-dashboard/data/laboratoria.json"
+function getData() {
+  fetch(link)
+    .then(response => response.json())
+    .then(laboratoria => {
+      console.log(laboratoria);
+})
+}
